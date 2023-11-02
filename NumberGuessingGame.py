7cfc4addbacba94d961879c9da49fac9d29c6157
Kyle64Bit2602 <142840056+Kyle64Bit2602@users.noreply.github.com>
2023-11-02 14:43:38 -0500

    Comments and Function Names

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -1,1 +1,25 @@
+import random
+import math
+import turtle
+
 # Number Guessing Game #
+# Main Functions #
+def main():
+  
+
+# Secondary Functions #
+def menu():
+    # Menu accepts no arguments
+    # User has three choices: Start game, range, and exit the program.
+def user_name(name1, name2):
+    # User_name accepts 2 arguments
+    # Users input their first names
+    # Program returns them
+def randomize(min, max):
+    # Randomize accepts two arguments
+    # Uses the arguments min and max to determine a range for a random number
+    random.randint(min, max)
+def guess(num):
+    # Guess accepts one argument
+    # Guess calls randomize and uses the random integer to determine a number
+    # User inputs their own number and the random integer and user's number are compared
